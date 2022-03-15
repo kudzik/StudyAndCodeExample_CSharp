80dc8717ea39951c8cb727139659e1b0dc3f60ee
Kudzik <kudzik@outlook.com>
2022-03-15 15:19:39 +0100

    Records

--- Records/Program.cs
+++ Records/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var person1 = new PersonRecord() { Name = "Person1", Age = 20 };
+
+var person2 = new PersonWithConstructor("Person2", 22);
+
+// Skopiowanie wszystkich wartości
+var person4 = person1 with { };
+// Nadpisanie wybranej wartości przy kopiowaniu
+var person5 = person1 with { Age = 44 };
+
+
+
+// Odczyt składowych rekordu
+var person1Name = person1.Name;
+var person1Age = person1.Age;
+
+// Dekonstrukcja
+var (Name, Age) = person2;
+
+public record PersonRecord
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+
+// rekord pozycyjny
+// kompilator automatycznie tworzy właściwości Name i Age, a akcesor set ustawiany jest na init
+public record PersonWithConstructor(string? Name, int Age);
